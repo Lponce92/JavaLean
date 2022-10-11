a4eab35f6bd6dd6b5dc21762c73d92eeeeca6ec1
Lponce92 <110445363+Lponce92@users.noreply.github.com>
2022-10-10 21:22:00 -0300

    g

---
+++ PuebasJs/Clase2/index.js
@@ -0,0 +1,77 @@
+console.log("INGRSO AL JS")
+
+// variables
+
+// var usuario = prompt("Ingrese su nombre de usuario")
+// console.log("usuario:"+usuario)
+
+// es mejor usar let
+// var nombre;
+let nombre = "Juan";
+console.log("nombre:"+nombre)
+
+// operar con var
+let a = 4
+let b = 5
+console.log("suma:" + (a+b) )
+
+// concatenar var string
+let nom="Juan"
+let ape="Suarez"
+let nomApe= nom + " " + ape
+console.log(nomApe)
+
+console.log("Bienvenido " + nom + " " + ape + " a nuestro sitio")
+console.log(`Bienvenido ${nom} ${ape} a nuestro sitio`)
+
+edad=36
+if (edad>=18) {
+    console.log("Es mayor de edad")
+} else {
+    console.log("No es mayor de edad")
+}
+
+//operadores logicos
+let usu 
+// usu = prompt("Ingrese su nombre de usuario")
+let pass 
+// pass = prompt("ingrese su password")
+
+if (usu=="nico@gmail.com" && pass=="1234") {
+    console.log("usu y pass correctos")
+} else {
+    console.log("usu y pass incorrectos")
+}
+
+// or ||
+let estalloviendo = false
+let haypronosticolluvia = true
+if (estalloviendo || haypronosticolluvia) {
+    console.log("llevar paraguas")
+} else {
+    console.log("No llevar paraguas")
+}
+
+// estructura de control
+// condicional
+
+if (edad >=18) [
+    // ingresarpagina
+]
+
+if (estalloviendo) {
+    console.log("Levar paraguas")
+} else {
+    console.log("No llevar paraguas")
+}
+
+edad=15
+if (edad <= 18) {
+    console.log("niño o joven")
+} else if (edad >18 && edad <= 60) {
+    console.log("adulto")
+} else {
+    console.log("anciano")
+}
+
+// var,let,const
