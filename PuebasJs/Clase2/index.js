e2dd6083f0348eeff5882bda51848af716bd8480
Lponce92 <110445363+Lponce92@users.noreply.github.com>
2022-10-18 14:55:08 -0300

    y

--- PuebasJs/Clase2/index.js
+++ PuebasJs/Clase2/index.js
@@ -8,36 +8,36 @@ console.log("INGRSO AL JS")
 // es mejor usar let
 // var nombre;
 let nombre = "Juan";
-console.log("nombre:"+nombre)
+console.log("nombre:" + nombre)
 
 // operar con var
 let a = 4
 let b = 5
-console.log("suma:" + (a+b) )
+console.log("suma:" + (a + b))
 
 // concatenar var string
-let nom="Juan"
-let ape="Suarez"
-let nomApe= nom + " " + ape
+let nom = "Juan"
+let ape = "Suarez"
+let nomApe = nom + " " + ape
 console.log(nomApe)
 
 console.log("Bienvenido " + nom + " " + ape + " a nuestro sitio")
 console.log(`Bienvenido ${nom} ${ape} a nuestro sitio`)
 
-edad=36
-if (edad>=18) {
+edad = 36
+if (edad >= 18) {
     console.log("Es mayor de edad")
 } else {
     console.log("No es mayor de edad")
 }
 
 //operadores logicos
-let usu 
+let usu
 // usu = prompt("Ingrese su nombre de usuario")
-let pass 
+let pass
 // pass = prompt("ingrese su password")
 
-if (usu=="nico@gmail.com" && pass=="1234") {
+if (usu == "nico@gmail.com" && pass == "1234") {
     console.log("usu y pass correctos")
 } else {
     console.log("usu y pass incorrectos")
@@ -55,7 +55,7 @@ if (estalloviendo || haypronosticolluvia) {
 // estructura de control
 // condicional
 
-if (edad >=18) [
+if (edad >= 18) [
     // ingresarpagina
 ]
 
@@ -65,13 +65,69 @@ if (estalloviendo) {
     console.log("No llevar paraguas")
 }
 
-edad=15
+edad = 15
 if (edad <= 18) {
     console.log("niño o joven")
-} else if (edad >18 && edad <= 60) {
+} else if (edad > 18 && edad <= 60) {
     console.log("adulto")
 } else {
     console.log("anciano")
 }
 
 // var,let,const
+if (true) {
+    var x = 3
+    let y = 5
+}
+
+const mayorEdad = 18
+// mayorEdad = 19
+
+const pi = 3.14
+
+// switch
+let nroMes = 4
+switch (nroMes) {
+    case 1:
+        console.log("Enero")
+        break;
+
+    case 2:
+        console.log("Febrero")
+        break;
+
+    case 3:
+        console.log("Marzo")
+        break;
+
+    case 4:
+        console.log("Abril")
+        break;
+
+    default:
+        console.log("No existe el mes")
+        break;
+}
+
+// iterador
+for (let i = 0; i < 4; i++) {
+    console.log(`iteración ${i}`)
+}
+
+// mientras
+let n = 0
+while (n < 4) {
+    console.log(n)
+    n++
+}
+
+while (usu != "nico@gmail.com" || pass != "1234") {
+    usu = prompt("Ingrese su nombre de usuario")
+    pass = prompt("ingrese su password")
+
+    if (usu == "nico@gmail.com" && pass == "1234") {
+        console.log("usu y pass correctos")
+    } else {
+        console.log("usu y pass incorrectos")
+    }
+}
---
+++ PuebasJs/Clase3/Validaciones.js
@@ -0,0 +1,35 @@
+
+function validarUsu() {
+    console.log("ingreso al validar usu")
+    let usu=document.getElementById("usu").value
+    console.log(usu)
+    
+    // DOM Document Object Model
+    if (usu.length==0) {
+        document.getElementById("msjUsu").innerHTML="Debe ingresar algún caractér"
+        console.log("Debe ingresar algún caractér")
+    } else if (usu.indexOf("@")==-1) {
+        document.getElementById("msjUsu").innerHTML="El usu debe contener un @"
+        console.log("El usu debe contener un @")
+    } else if (usu.indexOf("codoAcodo.gob.ar")==-1) {
+        document.getElementById("msjUsu").innerHTML="El usu debe ser del dominio codoAcodo.gob.ar"
+        console.log("El usu debe contener un @")    
+    } else {
+        document.getElementById("msjUsu").innerHTML="Usu correcto"
+        document.getElementById("msjUsu").className="bg-info rounded text-center my-2 px-2"
+        console.log("Usu correcto")
+    }
+}
+
+function updFortaleza() {
+    console.log("Ingreso updFort")
+    let pass=document.getElementById("pass").value
+    console.log(pass)
+    
+    if (pass.length<=4) {
+        document.getElementById("msjPass").innerHTML="Fortaleza Baja"
+    } else if(pass.length>4 && pass.length<=8){
+        document.getElementById("msjPass").innerHTML="Fortaleza Media"
+        document.getElementById("msjPass").className="bg-warning text-light text-center rounded my-2 px-2"
+    }
+}
